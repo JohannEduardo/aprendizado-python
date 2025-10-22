667646eb9c42ff8496f20f58b02dd73c7355d228
Johann Eduardo <johanneduardo200@gmail.com>
2025-10-21 21:08:56 -0300

    Conhecendo mais sobre funções com atividade básica

---
+++ Django Master/def.py
@@ -0,0 +1,30 @@
+# Ao invés de ficar repetindo linhas de código, crie funções
+
+def somar (a, b) :
+    resultado = a + b
+    return resultado
+
+# return nao e obrigatorio ser declarado
+
+# PRINT E BOM PARA APRENDIZADO, MUITAS VEZES UTILIZADOS NO INTITO DE MOSTRAR ALGO. MAS LEMBRAR SEMPRE QUE ELE E APENAS FORMA DE INTERAÇÃO COM O TERMINAL
+
+def envia_email (nome, email):
+    email = 'pycode@gmail.com'
+    senha = '12345'
+    nome_dest = nome
+    email_dest = email
+    # CONECTA PROVEDOR 
+    # MONTA CORPO DO E-MAIL
+    # ENVIA O EMAIL
+    return f'Email enviado para: {nome_dest} com o endereço de e-mail: {email_dest}!'
+
+pessoas = [
+    {'nome': 'Johann', 'email': 'johann@gmail.com'},
+    {'nome': 'Lucas', 'email': 'lucas@gmail.com'},
+    {'nome': 'Joana', 'email': 'joana@gmail.com'}
+]
+
+for pessoa in pessoas:
+    email_enviado = envia_email(pessoa['nome'], pessoa['email'])
+    print(email_enviado)
+#Nem sempre precisamos de parametros, as vezes o que precisamos pode ser encaixado dentro di bloco de codigo mesmo.
