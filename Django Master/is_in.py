1c9a9835c3d6950ebe28b766a8c6292f325a99e3
Johann <johanneduardo200@gmail.com>
2025-11-07 21:56:37 -0300

    Adicionando arquivos e atividades executadas no bootcamp de python na DIO

---
+++ Django Master/is_in.py
@@ -0,0 +1,18 @@
+#identidade
+
+#Se ocupam a mesma posição na memoria
+
+saldo = 1000
+limite = 500
+print(saldo is limite)
+
+#associacao
+
+#se esta presente em uma sequencia
+
+curso = 'Python Curso'
+frutas =[ 'Laranja', 'Banana', 'Maca']
+saques = [1000, 500]
+
+print('Python' in curso)
+print('Python' not in curso)
